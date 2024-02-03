f5e05258d3ff40062fcd375b7985ee5ba7afb1d0
Wyman <denisgorbatuk57@gmail.com>
2024-02-03 09:43:52 +0500

    Initial commit

---
+++ SPTaskThreeOne.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+#define y 2
+#define z 4
+#define t sin( 2 + z )
+#define x 6 * pow(t,2) - (z + 1) / pow(y,2)
+
+int main()
+{
+    cout << "y = " << y << endl << "z = " << z << endl << "t = " << t << endl << "x = " << x;
+}
+ 
